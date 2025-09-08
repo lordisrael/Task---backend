b093e0e6330d6d2408b24364b830a5a404e87ab4
lordisrael <josephisrael206@gmail.com>
2025-09-08 13:10:45 +0100

    First Commit

---
+++ config/jwt.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+
+const generateToken = (user) => {
+  return jwt.sign(
+    { id: user._id, email: user.email },
+    process.env.JWT_SECRET,
+    { expiresIn: process.env.JWT_LIFETIME }
+  );
+};
+
+const verifyToken = (token) => {
+  return jwt.verify(token, process.env.JWT_SECRET);
+};
+
+module.exports = { generateToken, verifyToken };
+
+
+
