6cafa434d7184b866d686d6f9dc10394b31f1068
lordisrael <josephisrael206@gmail.com>
2025-09-08 13:01:07 +0100

    First Commit

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 import express from 'express';
 import cors from 'cors';
 import dbConnect from './config/dbConnect';
+import notFound from './middleware/not-found';
+import taskRoute from './route/taskRoute';
+import authRoute from './route/authRoute';
+import dotenv from 'dotenv';
 
-
-
-
+dotenv.config();
+const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/tasks", taskRoute);
+
+
+
+app.use(notFound);
 
 
 const start =  async () => {
